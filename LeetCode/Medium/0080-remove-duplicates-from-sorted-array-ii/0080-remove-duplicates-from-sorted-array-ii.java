8481861cf91a37fa0d5ed27a483a314703cadb6e
Vedang <vedangsolaskar24@gmail.com>
2026-02-05 12:10:11 +0530

    Time: 0 ms (100%), Space: 48.9 MB (26.05%) - LeetHub

--- LeetCode/Medium/0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ LeetCode/Medium/0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,27 +1,17 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int i = 0;
-        int j = 1;
-        int last = nums[0];
-        boolean onlyOne = true;
+        int i = 2;
+        int j = 2;
+
         while (j < nums.length ) {
-
-            if(nums[j] == last && onlyOne) {
-                nums[i+1] = nums[j];
-                onlyOne = false;
-                i++;     
-            }
-
-            if(nums[j] != nums[j-1]){
-                nums[i+1] = nums[j];
-                onlyOne = true;
+            if(nums[j] != nums[i-2]) {
+                nums[i] = nums[j];
                 i++;
-            } 
-            last = nums[i];
+            }
             j++;
         }
 
-        return i+1;
+        return i;
 
     }
 }
