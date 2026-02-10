4d01acdbc3c7aa7a350cc394845ce1fdad861e10
Vedang <vedangsolaskar24@gmail.com>
2026-02-10 11:17:34 +0530

    Time: 36 ms (32.29%), Space: 59.1 MB (73.59%) - LeetHub

--- LeetCode/Medium/0015-3sum/0015-3sum.java
+++ LeetCode/Medium/0015-3sum/0015-3sum.java
@@ -2,19 +2,30 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
         Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
 
-        Set<List<Integer>> ans = new HashSet<>();
-        for(int i = 0; i < nums.length; i++) {
-            int start = i+1;
-            int end = nums.length-1;
+        for (int i = 0; i < nums.length - 2; i++) {
+            int start = i + 1;
+            int end = nums.length - 1;
 
-            while(start < end) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            while (start < end) {
+
                 int sum = nums[i] + nums[start] + nums[end];
-                if(sum == 0) {
+
+                if (sum == 0) {
                     ans.add(new ArrayList<>(Arrays.asList(nums[i], nums[start], nums[end])));
+
+                    while (start < end && nums[start] == nums[start + 1])
+                        start++;
+                    while (start < end && nums[end] == nums[end - 1])
+                        end--;
+
                     start++;
                     end--;
-                } else if( sum < 0) {
+                } else if (sum < 0) {
                     start++;
                 } else {
                     end--;
@@ -22,6 +33,6 @@ class Solution {
             }
         }
 
-        return new ArrayList<> (ans);
+        return new ArrayList<>(ans);
     }
 }
